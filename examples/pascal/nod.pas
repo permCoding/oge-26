d84924b21b37d45edf089fc0209b42c8064f5673
permCoding <ttxiom@gmail.com>
2025-09-15 16:20:31 +0500

    up

---
+++ examples/pascal/nod.pas
@@ -0,0 +1,31 @@
+﻿var x,y,r: integer;
+
+procedure nod(a,b: integer; var result: integer);
+begin
+  while a <> b do
+  begin
+    if a > b then a := a - b else b := b - a;
+  end;
+  result := a;
+end;
+
+procedure nod_mod(a,b: integer; var result: integer);
+begin
+  while a*b <> 0 do
+  begin
+    if a > b then a := a mod b else b := b mod a;
+  end;
+  result := a+b;
+end;
+
+begin
+
+  x := 49; y := 21;
+  
+  nod(x, y, r);
+  writeln('НОД(', x, ', ', y, ') = ', r);
+
+  nod_mod(x, y, r);
+  writeln('НОД(', x, ', ', y, ') = ', r);
+
+end.
