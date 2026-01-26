f06cd0a1c825d8937d92ca984c03ad269385739a
permCoding <ttxiom@gmail.com>
2026-01-26 18:30:40 +0500

    p

---
+++ days/day-09-2026-01-26/10.py
@@ -0,0 +1,30 @@
+n = 17
+
+print(n / 2)   # 8.5
+print(n // 2)  # 8
+print(n % 2)   # 1
+
+n = 17
+d = 3
+print(n /  d)   # 5.__
+print(n // d)   # 5
+print(n %  d)   # 2
+
+n = 178
+d = 10
+print(n /  d)   # 17.8
+print(n // d)   # 17
+print(n %  d)   # 8   <===
+
+n = 17
+d = 10
+print(n /  d)   # 1.7
+print(n // d)   # 1
+print(n %  d)   # 7   <===
+
+n = 1
+d = 10
+print(n /  d)   # 0.1
+print(n // d)   # 0
+print(n %  d)   # 1   <===
+
