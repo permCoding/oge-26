e048ede54f5ef049ef13cce8e4ad27710f58f012
permCoding <ttxiom@gmail.com>
2026-05-01 18:28:46 +0500

    p

---
+++ days/day-2026-04-28/16-37830+.py
@@ -0,0 +1,20 @@
+k = int(input())
+m = 0
+for i in range(k):  # 0 1 k-1
+    n = int(input())
+    if n%5 == 0 and n > m:
+        m = n
+print(m)  # максимальное число, кратное 5
+
+
+
+
+##нц k раз
+##
+##кц
+
+
+
+
+
+# float(input())
---
+++ days/day-2026-04-28/16-37830.py
@@ -0,0 +1,21 @@
+k = int(input())
+m = 0
+for i in range(k):  # 0 1 k-1
+    n = int(input())
+    if n%5 == 0:
+        if n > m:
+            m = n
+print(m)  # максимальное число, кратное 5
+
+
+
+
+##нц k раз
+##
+##кц
+
+
+
+
+
+# float(input())
